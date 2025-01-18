ec0efb36c287e419d7699f6a9449b63f6a4ccf9b
wolfstylee <amnack71588@gmail.com>
2025-01-18 16:30:42 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.ma
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
-  'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9',
+  'DoHkbYJPt59J9MHq5bbVnqcq22i4YkBJGB6CzEbJTFy5',
 )
 
 // Gamba explorer URL - Appears in RecentPlays
